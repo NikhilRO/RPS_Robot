88fb24efec197d162ce3e013de2817b89357ca52
maxgao11111 <maxgao18@yahoo.com>
2017-11-04 15:43:56 -0400

    added methods

--- Neural Net/Net.py
+++ Neural Net/Net.py
@@ -1,6 +1,8 @@
 import random
 import math
 
+# -- The following methods are mathematical methods used by the neural network
+
 # Returns array rounded to n decimals
 def arr_round (arr, decimals):
     return [round(e, decimals) for e in arr]
@@ -67,6 +69,7 @@ def sigmoid_prime_v(vec_a):
         z2.append(sigmoid_prime(vec_a[_]))
     return z2
 
+# -- Class for the neural network
 class Net:
     def __init__(self, layer_sizes):
         self.__layer_sizes = layer_sizes
@@ -81,10 +84,6 @@ class Net:
             # self.weights.append([[0 for _ in range(layer_sizes[x - 1])] for __ in range(layer_sizes[x])])
             self.__biases.append([random.uniform(-2, 2) for _ in range(layer_sizes[x])])
             self.__weights.append([[random.uniform(-2, 2) for _ in range(layer_sizes[x-1])] for __ in range (layer_sizes[x])])
-
-    # Returns all weights exiting from a given layer
-    def get_weights(self, layer):
-        return self.__weights[layer]
 
     # Returns all weights leading out of a neuron
     def __exiting_weights(self, layer, neuron):
@@ -110,35 +109,6 @@ class Net:
             input_layer = sigmoid_v(self.__next_activation(input_layer, l))
         return input_layer
 
-    #Returns fraction correct by testing it against the neural net
-    def evaluate(self, test_data):
-        n_tests = len(test_data)
-        tests = [0 for x in range(n_tests)]
-        expected = [0 for x in range(n_tests)]
-        for a in range(n_tests):
-            tests[a] = test_data[a][0]
-            expected[a] = test_data[a][1]
-        results = []
-        for _ in range(n_tests):
-            results.append(self.feed_forward(tests[_]))
-
-        correct = 0.0
-        for t in range(n_tests):
-            max = -1
-            ind = 0
-            for c in range(0, len(results[t])):
-                if results[t][c] > max:
-                    max = results[t][c]
-                    ind = c
-            res = 0
-            for c in range(1, len(expected[t])):
-                if expected[t][c] == 1:
-                    res = c
-            if res == ind:
-                correct += 1
-
-        return correct/n_tests;
-
     # Returns the cost derivative, calculated as dCost/dActiv = activ - expect (since C = .5sum(exp-activ)^2)
     def __cost_derivative(self, output_activation, expected_val):
         return add_vec(1, -1, output_activation, expected_val)
@@ -161,12 +131,13 @@ class Net:
             activation_vecs.append(sigmoid_v(z))
             activation_vecs_prime.append(sigmoid_prime_v(z))
             z = sigmoid_v(z)
-        # Gradient of biases is 2D and weights is 3D
+        # Gradient of biases are a 2D array and weights are a 3D array
         # Gradient of biases is a 2D array b[l][n] storing the bias of layer l-1 and neuron n-1
         grad_b = [0 for l in range(1, self.__n_layers)]
         # Gradient of weights is a 3D array w[l][n][p] storing the weight connecting neuron p-1 on layer l-1 to neuron n-1 on layer l
         grad_w = [[0 for n in range(self.__layer_sizes[l])] for l in range(1, self.__n_layers)]
 
+        # Initial error hadamard(d_cost, sig_prime)
         error = hadamard_prod(self.__cost_derivative(activation_vecs[self.__n_layers-1], expected), activation_vecs_prime[self.__n_layers-1])
         grad_b[self.__n_layers-2] = error
         for n in range(self.__layer_sizes[self.__n_layers-1]):
@@ -195,24 +166,69 @@ class Net:
         self.__biases = [add_vec(1, -avg_step, self.__biases[a], grad_b[a]) for a in range(self.__n_layers-1)]
         self.__weights = [[add_vec(1, -avg_step, self.__weights[a][b], grad_w[a][b]) for b in range(self.__layer_sizes[a+1])] for a in range(self.__n_layers-1)]
 
+    # Returns fraction correct by testing it against the neural net
+    def evaluate(self, test_data):
+        n_tests = len(test_data)
+        tests = [0 for x in range(n_tests)]
+        expected = [0 for x in range(n_tests)]
+
+        for a in range(n_tests):
+            tests[a] = test_data[a][0]
+            expected[a] = test_data[a][1]
+        results = []
+
+        for _ in range(n_tests):
+            results.append(self.feed_forward(tests[_]))
+        correct = 0.0
+
+        for t in range(n_tests):
+            max = -1
+            ind = 0
+            for c in range(0, len(results[t])):
+                if results[t][c] > max:
+                    max = results[t][c]
+                    ind = c
+            res = 0
+            for c in range(1, len(expected[t])):
+                if expected[t][c] == 1:
+                    res = c
+            if res == ind:
+                correct += 1
+
+        return correct / n_tests;
+
     # Performs SGD to network
-    def stochastic_gradient_descent(self, epochs, mini_batch_size, training_inputs, expected_outputs, step_size, validation_data=None):
-        tests = []
+    def stochastic_gradient_descent(self, epochs, mini_batch_size, training_inputs, expected_outputs, step_size, test_input=None, test_output=None):
+        training_data = []
         for t in range(len(training_inputs)):
-            tests.append([training_inputs[t], expected_outputs[t]])
-        if len(tests) == 0:
+            training_data.append([training_inputs[t], expected_outputs[t]])
+        if len(training_data) == 0:
             return
 
+        test_data = []
+        if test_input:
+            for i, o in zip(test_input, test_output):
+                test_data.append([i, o])
+
         for iters in range(epochs):
-            random.shuffle(tests)
-            mini_batches = [tests[curr:curr+mini_batch_size] for curr in range(0, len(tests), mini_batch_size)]
+            random.shuffle(training_data)
+            mini_batches = [training_data[curr:curr+mini_batch_size] for curr in range(0, len(training_data), mini_batch_size)]
             for batch in mini_batches:
                 self.__update_net_weights_biases(batch, step_size)
 
-            if validation_data:
-                print("Epoch", iters+1, " percent correct", self.evaluate(validation_data))
+            if test_input:
+                print("Epoch", iters+1, " percent correct", self.evaluate(test_data))
             else:
-                print("Epoch", iters + 1, " percent correct", self.evaluate(mini_batches[0]))
+                print("Epoch", iters+1, " percent correct", self.evaluate(mini_batches[0]))
+
+    # Returns all weights in the neural network (3D Array)
+    def get_weights(self):
+        return self.__weights
+
+    # Returns all biases in the neural network (2D Array)
+    def get_biases(self):
+        return self.__biases
+
             #if iters+1 == epochs:
                 # print (arr_round(self.feed_forward([1,1,1]), 2)) #out 0
                 # print (arr_round(self.feed_forward([0,0,0]), 2)) #out 1
--- Neural Net/NeuralNet.py
+++ Neural Net/NeuralNet.py
@@ -5,23 +5,30 @@ def new_net (layer_sizes):
     return neuralnet.Net(layer_sizes)
 
 # Pass through neural network (net), epochs (number of training loops), training inputs (list of inputs (1D arrays))
-#   expected outputs (list of expected outputs), step size, validation data (as a 2-tuple, (training inputs, expected outputs)
+#   expected outputs (list of expected outputs), step size, validation data (as a list of 2-tuples, (training inputs, expected outputs))
 def train_net(net, epochs, mini_batch_size, training_inputs, expected_outputs, step_size, validation_data=None):
     net.stochastic_gradient_descent(epochs, mini_batch_size, training_inputs, expected_outputs, step_size, validation_data)
 
 # Returns the index of the output given an input
+# eg: if out_from_net = [0.11, 0.2, 0.9]
+# method returns: ind = 2
 def get_output (net, input):
     out = net.feed_forward(input)
-
     ind = 0
     max = out[0]
     for x in range(1, len(out)):
         if out[x] > max:
             ind = x
             max = out[x]
-
     return ind
 
+# Returns the weights in the network (3D array)
+def get_weights(net):
+    return net.get_weights()
+
+# Returns the biases in the network (2D array)
+def get_biases(net):
+    return net.get_biases()
 
 
 #EXAMPLE:
